805d4af96e1deba52f0bf58106149d6df8afcd30
Dmytro Halushkov <halushkovdmitry93@gmail.com>
2022-05-05 17:56:08 +0300

    adding additional items and scrolling the page

--- Functions.js
+++ Functions.js
@@ -1,4 +1,15 @@
-import {INPUT_BOX , MAIN_CONTAINER , RECENT_ARRAY , BEER_LIST , REGEX , RECENT_CONTAINER } from "./Variables.js";
+import {
+    INPUT_BOX,
+    MAIN_CONTAINER,
+    RECENT_ARRAY,
+    BEER_LIST,
+    REGEX,
+    RECENT_CONTAINER,
+    ADD_MORE_BTN,
+    NO_MORE_DATA,
+    STYLE,
+    VALIDATION
+} from "./Variables.js";
 import {CreateBeer} from './CreateBeer.js';
 
 let pageStart = 1;
@@ -12,27 +23,31 @@ export function startSearch(){
     getBeer();
     INPUT_BOX.placeholder = INPUT_BOX.value;
     INPUT_BOX.value = '';
+    NO_MORE_DATA.style.display = STYLE.DISPLAY.NONE;
     window.scrollTo(0,100 + RECENT_ARRAY.length * 40);
 }
 
-export function declineSearch() {
-    INPUT_BOX.style.color = 'rgba(246, 15, 15, 0.622)';
-    INPUT_BOX.style.fontWeight = 'bold';
-    INPUT_BOX.style.border = '1px solid rgba(246, 15, 15, 0.622)';
-    INPUT_BOX.style.backgroundColor = 'rgba(215,24,24,0.57)';
-    
-    setTimeout(() => {
-        INPUT_BOX.style.color = 'black';
-        INPUT_BOX.style.fontWeight = 'normal';
-        INPUT_BOX.style.border = 'none';
-        INPUT_BOX.style.backgroundColor = 'white';
-    },500);
-};
+export function declineSearch(){
+    inputValidation(INPUT_BOX, VALIDATION.INVALID)
+
+    setTimeout( () => {
+        inputValidation(INPUT_BOX, VALIDATION.VALID)
+    }, 500)
+}
+
+export function inputValidation(input, validation){
+    input.style.color = validation.color;
+    input.style.fontWeight = validation.fontWeight;
+    input.style.border = validation.border;
+    input.style.backgroundColor = validation.backgroundColor;
+}
 
 export function createRecentSearch(){
     const uniqueArray = Array.from(new Set(RECENT_ARRAY))
 
-    if (uniqueArray.includes(INPUT_BOX.value)) {
+    const isIncludes = uniqueArray.includes(INPUT_BOX.value.toLowerCase())
+
+    if (isIncludes) {
         return
     }
 
@@ -45,15 +60,17 @@ export function createRecentSearch(){
 export function getBeer() {
     const value = INPUT_BOX.value.replaceAll(' ', '_').trim();
 
-    fetch(`https://api.punkapi.com/v2/beers?beer_name=${value}&page=${pageStart}&per_page=80`)
+    pageStart = 1;
+    fetch(`https://api.punkapi.com/v2/beers?beer_name=${value}&page=${pageStart}&per_page=5`)
         .then((response) => response.json())
         .then((result) => {
+            MAIN_CONTAINER.innerHTML = '';
 
             if (!result.length){
                 return MAIN_CONTAINER.innerHTML = CreateBeer.getError();
             }
 
-            MAIN_CONTAINER.innerHTML = '';
+            ADD_MORE_BTN.style.display = STYLE.DISPLAY.BLOCK;
             BEER_LIST.length = 0;
             showBeerList(result)
         })
@@ -68,10 +85,42 @@ export function showBeerList(source){
             description: `${item.description}`
         });
 
-        BEER_LIST.push(newBeer)
-        MAIN_CONTAINER.innerHTML += newBeer.getInnerHtml()
+        BEER_LIST.push(newBeer);
+        MAIN_CONTAINER.innerHTML += newBeer.getInnerHtml();
     })
 }
 
+export function additionalItems(){ //adding 5 more items to the list
+    const value = INPUT_BOX.placeholder;
+
+    pageStart +=  5;
+    fetch(`https://api.punkapi.com/v2/beers?page=${pageStart}&per_page=5&beer_name=${value}`)
+        .then((response) => response.json())
+        .then((result) => {
+            if (!result.length){
+                warningModal();
+
+                if (CreateBeer.getError()){
+                    return
+                }
+
+            }
+
+            showBeerList(result)
+        })
+};
+
+export function warningModal(){
+    showModal(STYLE.DISPLAY.FLEX, STYLE.OPACITY.FALSE)
+
+    setTimeout(() => {
+        showModal(STYLE.DISPLAY.NONE, STYLE.OPACITY.TRUE)
+    }, 2000)
+}
+
+export function showModal(modalDisplay, buttonOpacity){
+    ADD_MORE_BTN.style.opacity = buttonOpacity;
+    NO_MORE_DATA.style.display = modalDisplay;
+}
 
 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import {INPUT_BOX , KEYCODE, RECENT_CONTAINER , SEARCH_BTN } from "./Variables.js"
-import {validInput, declineSearch, startSearch} from './Functions.js'
+import {ADD_MORE_BTN, INPUT_BOX, KEYCODE, RECENT_ARRAY, RECENT_CONTAINER, SCROLL_BTN, SEARCH_BTN} from "./Variables.js"
+import {validInput, declineSearch, startSearch, additionalItems} from './Functions.js'
 
-inputBox.addEventListener('keyup',  function onKeyValidation(e){
+INPUT_BOX.addEventListener('keyup',  function onKeyValidation(e){
     if ((e.keyCode !== KEYCODE.ENTER) || (!INPUT_BOX)){
         return
     }
@@ -15,7 +15,7 @@ inputBox.addEventListener('keyup',  function onKeyValidation(e){
     }
 });
 
-searchBtn.addEventListener('click', function onClickValidation(e){
+SEARCH_BTN.addEventListener('click', function onClickValidation(e){
     const isValid = validInput(INPUT_BOX.value);
 
     if (isValid) {
@@ -25,14 +25,29 @@ searchBtn.addEventListener('click', function onClickValidation(e){
     }
 });
 
-recentContainer.addEventListener('click', function startRecentSearch(e) {
+RECENT_CONTAINER.addEventListener('click', function startRecentSearch(e) {
     const recentElMatch = e.target.classList.contains('recentEl')
     
-    if (recentElMatch){
-        INPUT_BOX.value = e.target.innerText;
-        startSearch();
+    if (!recentElMatch){
+        return
     }
+
+    INPUT_BOX.value = e.target.innerText;
+    startSearch();
 });
 
+window.onscroll = () => {
+    if (window.scrollY > 400) {
+        SCROLL_BTN.classList.remove('arrowUpHide')
+    } else if (window.scrollY < 400) {
+        SCROLL_BTN.classList.add('arrowUpHide')
+    }
+}
+
+SCROLL_BTN.addEventListener('click', function navigateToFirstEl(){
+    window.scrollTo(0,100 + RECENT_ARRAY.length * 40)
+})
+
+ADD_MORE_BTN.addEventListener('click', additionalItems);
 
 
