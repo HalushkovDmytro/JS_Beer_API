64780205be455baacb0c695f1bf5273498f39337
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-05-05 13:24:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import {inputBox, KEYCODE, recentContainer, searchBtn} from "./Variables.js"
+import {INPUT_BOX , KEYCODE, RECENT_CONTAINER , SEARCH_BTN } from "./Variables.js"
 import {validInput, declineSearch, startSearch} from './Functions.js'
 
 inputBox.addEventListener('keyup',  function onKeyValidation(e){
     if ((e.keyCode !== KEYCODE.ENTER) || (!inputBox)){
         return
     }
+    
     const isValid = validInput(inputBox.value);
 
     if (isValid) {
@@ -25,7 +26,9 @@ searchBtn.addEventListener('click', function onClickValidation(e){
 });
 
 recentContainer.addEventListener('click', function startRecentSearch(e) {
-    if(e.target.classList.contains('recentEl')){
+    const recentElMatch = e.target.classList.contains('recentEl')
+    
+    if (recentElMatch){
         inputBox.value = e.target.innerText;
         startSearch();
     }
